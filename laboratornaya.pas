2e8a9d0ca3775a6eb79d49d56b35d15bc66246a2
kirill864 <74341632+kirill864@users.noreply.github.com>
2023-04-23 16:25:42 +0300

    Add files via upload

---
+++ laboratornaya.pas
@@ -0,0 +1,12 @@
+﻿program ThreeDigitsNoRepeated;
+
+var
+  i, j, k: integer;
+
+begin
+  for i := 1 to 9 do
+    for j := 0 to 9 do
+      for k := 0 to 9 do
+        if (i <> j) and (i <> k) and (j <> k) then
+          writeln(i * 100 + j * 10 + k);
+end.
